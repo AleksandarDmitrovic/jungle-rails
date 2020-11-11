5a21fbbda4bdb22a440403d266b8ab2a512e3ae8
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-11 10:52:18 -0700

    Admin categories page with unique product count

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,7 +2,8 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
 
   def index
-    @categories = Category.order(id: :desc).all
+    @category_array = Category.order(id: :desc).all
+    @categories = @category_array.map { |category| { id: category.id, name: category.name, product_count: Product.where(category_id: category.id).count}}
   end
 
   def new
