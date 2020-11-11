2f0404c39d03d846de699f3f77c1f4cd61a10a47
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-11 14:35:48 -0700

    update login routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,10 +19,9 @@ Rails.application.routes.draw do
 
   resource :about, only: [:show]
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+  resources :login, only: [:new, :create]
 
+  get '/logout' => 'sessions#destroy'
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
