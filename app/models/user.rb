c972d3980488509427b2a3cdf5f33be1885dbfd4
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-14 14:32:15 -0700

    Refactor validation on User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ActiveRecord::Base
   
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true , uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 8 }
-  validates :email, uniqueness: { case_sensitive: false }
 
   def self.authenticate_with_credentials(email, password)
     email = email.strip.downcase
