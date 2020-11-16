96687df0f31332834ef07c634fbbde10caaff567
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-16 13:43:36 -0700

    Create view template for html format email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: "no-reply@jungle.com"
+
+  def recepit_email
+    @user = params[:user]
+    @url = "http://localhost/3000/orders/id"
+    mail(to: @user.email, subject: "Thank you for your order (order number)")
+  end
 end
