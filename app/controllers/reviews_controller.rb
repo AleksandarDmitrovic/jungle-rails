1a3010664c0125ccaa10ca11e25fb6e7ebbf6b0c
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-17 16:15:16 -0700

    Add create logic for new reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,28 @@
 class ReviewsController < ApplicationController
 
+  before_filter :authorize
+
   def create
-    raise "Yay, I'm here!"
-  end
+    # @review = Product.where().has_many
+    @review = Review.new(review_params)
+    @review.user_id = session[:user_id]
+    @review.product_id = params[:product_id]
   
+    if @review.save
+      redirect_to "/products/#{params[:product_id]}", notice: 'Review created!'
+    else
+      redirect_to "/products/#{params[:product_id]}"
+    end
+
+  end
+
+  private
+
+  def review_params
+    params.require(:products).permit(
+      :description,
+      :rating
+    )
+  end
+
 end
